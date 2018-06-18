49bfc069d062cfa4252a4d72015f9cd5f00a4080
Armbrust Samuel <samuel.armbrust@sap.com>
2018-06-18 03:55:33 +0000

    Initial functions for Visitors view

--- webapp/controller/Visitors.controller.js
+++ webapp/controller/Visitors.controller.js
@@ -1,9 +1,30 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"../model/formatter",
+	"sap/m/MessageToast"
+], function(Controller, formatter, MessageToast) {
 	"use strict";
 
 	return Controller.extend("com.sapvisitors-techschool-1st.controller.Visitors", {
+		
+		formatter: formatter,
+		
+		onItemClose: function (oEvent) {
+			var oItem = oEvent.getSource(),
+				oList = oItem.getParent();
 
+			oList.removeItem(oItem);
+
+			MessageToast.show('Item Closed: ' + oEvent.getSource().getTitle());
+		},
+		
+		onAcceptPress: function(e){
+			MessageToast.show("You have accepted the visitor");
+		},
+		
+		onRejectPress: function(e){
+			MessageToast.show("You have rejected the visitor");
+		}
+		
 	});
 });
